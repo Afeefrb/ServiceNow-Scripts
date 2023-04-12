0243f2d961f1dd9a53cb675bab7ae376b4487961
Afeef Rabbani <52856436+Afeefrb@users.noreply.github.com>
2023-04-12 13:58:31 +0530

    Update priority-1_incident_alertBox.js

--- priority-1_incident_alertBox.js
+++ priority-1_incident_alertBox.js
@@ -3,10 +3,10 @@
 When any incident record is opened or created, when the priority of the incident is changed to 1, show an alert box.
 **/
 
-1. All > System Definition > Client Scripts : New, OR
-2. Incident List > List context menu > Configure > Client Scripts 
-3. Table: [incident], Type: onChange, Field name Priority
-4. Script:
+// 1. All > System Definition > Client Scripts : New, OR
+// 2. Incident List > List context menu > Configure > Client Scripts 
+// 3. Table: [incident], Type: onChange, Field name Priority
+// 4. Script:
 
 function onChange(control, oldValue, newValue, isLoading, isTemplate){
    if(isLoading || newValue === ""){
@@ -19,6 +19,6 @@ function onChange(control, oldValue, newValue, isLoading, isTemplate){
    }
 }
 
-5. All > Incident > All : Click on an Incident Form
-6. Output: Alert box pops up => "This is a P1 incident!"
-Note: The alert box pops up before the priority value is changed. When “Ok” is clicked and the box is closed, then the priority value changes
+// 5. All > Incident > All : Click on an Incident Form
+// 6. Output: Alert box pops up => "This is a P1 incident!"
+// Note: The alert box pops up before the priority value is changed. When “Ok” is clicked and the box is closed, then the priority value changes
